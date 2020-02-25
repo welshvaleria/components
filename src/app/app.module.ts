f5005602e6412b3e6364cf5d24b11ebefca6f935
emullendore <ekmullendore@gmail.com>
2020-02-24 19:54:13 -0600

    added pizza-toppings component and pizza service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    PizzaToppingsComponent
   ],
   imports: [
     BrowserModule
--- src/app/pizza-toppings/pizza-toppings.component.spec.ts
+++ src/app/pizza-toppings/pizza-toppings.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestComponent } from './test.component';
+import { PizzaToppingsComponent } from './pizza-toppings.component';
 
-describe('TestComponent', () => {
-  let component: TestComponent;
-  let fixture: ComponentFixture<TestComponent>;
+describe('PizzaToppingsComponent', () => {
+  let component: PizzaToppingsComponent;
+  let fixture: ComponentFixture<PizzaToppingsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestComponent ]
+      declarations: [ PizzaToppingsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestComponent);
+    fixture = TestBed.createComponent(PizzaToppingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
