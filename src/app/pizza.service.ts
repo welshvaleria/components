562ccb8080dcc3dc46c3b54c64fc4fb61c1a660e
sdmiller1 <54753630+sdmiller1@users.noreply.github.com>
2020-02-24 20:07:17 -0600

    wrote out dummy loadpizzatoppings function

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  loadPizzaToppings() {
+    const pizzaToppings = [
+      {name: "pepperoni", price: 9.99}
+      ,{name: "Cheese", price: 5.99}
+      ,{name: "Sausage", price: 6.99}
+      ,{name: "Bacon", price: 2.99}
+    ];
+
+    return pizzaToppings.map(x => ({ ...x, checked: false }));
+  }
 }
