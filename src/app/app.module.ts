c380e3c41d33929ae8290589e7666d7fe65529fc
Twijn <twijn@twijn.net>
2020-02-24 19:39:02 -0600

    Generated pizza-toppings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaToppingsComponent
   ],
   imports: [
     BrowserModule
