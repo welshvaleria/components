cd0616e2cf08d1bad6a4ea370121cec56bd7026b
aneevel <aneevel15@gmail.com>
2020-02-24 19:59:52 -0600

    Inject pizza service into pizza-toppings component

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  // Magic Dependency Injection... using great TypeScript syntax!
+  constructor(private pizzaSvc: PizzaService) { }
 
   ngOnInit(): void {
   }
