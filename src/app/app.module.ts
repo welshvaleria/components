079536b73f0152bc60ba682353599e116d44a49c
sdmiller1 <54753630+sdmiller1@users.noreply.github.com>
2020-02-24 20:25:47 -0600

    did some stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,6 +11,7 @@ import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.componen
   ],
   imports: [
     BrowserModule
+    , FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
