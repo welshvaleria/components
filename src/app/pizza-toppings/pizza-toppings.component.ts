bf6b64cec69800bd36c3c827bc43ee59972319ed
sdmiller1 <54753630+sdmiller1@users.noreply.github.com>
2020-02-24 19:59:54 -0600

    dependency injected the pizza service into the pizza toppings component

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  // Magic DI (Dependency injection)
+  constructor(private pizzaSvc: PizzaService) { }
 
   ngOnInit(): void {
   }
