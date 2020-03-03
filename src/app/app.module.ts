97ff9431db9bd6ea8e04849f0d25e248f13aeed7
jacobesoper <50640194+jacobesoper@users.noreply.github.com>
2020-03-02 19:51:45 -0600

    Initial commit of jacob's component
    ng generate component jacobs-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
-import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { JacobsComponentComponent } from './jacobs-component/jacobs-component.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    JacobsComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/jacobs-component/jacobs-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jacobs-component',
+  templateUrl: './jacobs-component.component.html',
+  styleUrls: ['./jacobs-component.component.css']
+})
+export class JacobsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
