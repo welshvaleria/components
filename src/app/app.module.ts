9c48e9155154c0550381a7d0545d7297f31017df
cjusti2 <crobichaux@madisoncollege.edu>
2020-03-02 19:32:38 -0600

    added checkbox
    added checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import {MatCardModule} from '@angular/material/card'
 
+import {MatButtonModule} from '@angular/material/button'
+
+import {MatCheckboxModule} from '@angular/material/checkbox'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +21,9 @@ import {MatCardModule} from '@angular/material/card'
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
