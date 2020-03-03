3098dc033b17ac6e85390033c93f558283a78cbc
timzschernitz <37000135+timzschernitz@users.noreply.github.com>
2020-03-02 19:31:07 -0600

    Added material checkboxes
    Updated the checkboxes to be facny looking and interactive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.componen
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { MatCardModule } from '@angular/material/card';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
