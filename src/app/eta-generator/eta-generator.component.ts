057cc6bd085bd3dc2fd9176f204938449aab01d5
Twijn <twijn@twijn.net>
2020-04-22 16:52:25 -0500

    Add generator code
    Create the worst, absolutely horrible, generator code for creating dates. This is honestly the worst thing I've ever done.

--- src/app/eta-generator/eta-generator.component.ts
+++ src/app/eta-generator/eta-generator.component.ts
@@ -9,7 +9,36 @@ export class EtaGeneratorComponent implements OnInit {
 
   constructor() { }
 
+  date = "Click above to generate a date!";
+
+  dateOptions = [
+    {
+      monthName: "April"
+      , min: new Date().getDate()
+      , max: 30
+    }
+    , {
+      monthName: "May"
+      , min: 1
+      , max: 15
+    }
+  ]
+
   ngOnInit(): void {
+  }
+
+  generateDate() {
+    let monthOption;
+
+    if (Math.random() >= .5 && new Date().getMonth() == 3) {
+      monthOption = this.dateOptions[0];
+    } else {
+      monthOption = this.dateOptions[1];
+    }
+
+    let day = Math.floor((monthOption.max - monthOption.min) * Math.random()) + monthOption.min;
+
+    this.date = monthOption.monthName + " " + day;
   }
 
 }
